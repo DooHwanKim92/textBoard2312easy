2bd46cea05934de4441bf100f888f7fefe20c264
DooHwanKim92 <rmfpsvldj@gamil.com>
2023-12-11 17:59:38 +0900

    dya-2-2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -10,10 +11,13 @@ public class App {
         System.out.println("===== 명언 앱 =====");
 
         Scanner sc = new Scanner(System.in);
+
         List<WiseSaying> wiseSayingList = new ArrayList<>();
-        List<Member> memberList = new ArrayList<>();
         int id = 1;
+        List<Member> memberList = new ArrayList<>();
         int userPrimaryId = 1;
+
+        Member loginedMember = null;
 
         while (true) {
             System.out.print("명령 > ");
@@ -22,49 +26,74 @@ public class App {
                 System.out.println("===== 시스템 종료 =====");
                 break;
             } else if (command.equals("등록")) {
+                if (loginedMember == null) {
+                    System.out.println("로그인이 필요합니다.");
+                    continue;
+                }
                 System.out.print("명언(등록) : ");
                 String content = sc.nextLine().trim();
                 System.out.print("작가(등록) : ");
                 String author = sc.nextLine().trim();
+                String userId = loginedMember.userId;
 
-                WiseSaying ws = new WiseSaying(id, content, author);
+                WiseSaying ws = new WiseSaying(id, content, author, userId);
                 wiseSayingList.add(ws);
 
                 System.out.println(id + "번 명언 등록 완료!!");
                 id++;
             } else if (command.equals("목록")) {
-                System.out.println("  번호  /  작가  /  명언  ");
+                if (loginedMember == null) {
+                    System.out.println("로그인이 필요합니다.");
+                    continue;
+                }
+                System.out.println("  번호  /  작가  /  명언  / 작성자  ");
                 System.out.println("------------------------");
                 for (WiseSaying ws : wiseSayingList) {
-                    System.out.println(ws.getId() + " / " + ws.getAuthor() + " / " + ws.getContent());
+                    System.out.println(ws.getId() + " / " + ws.getAuthor() + " / " + ws.getContent() + " / " + ws.getUserId());
                 }
             } else if (command.equals("삭제")) {
+                if (loginedMember == null) {
+                    System.out.println("로그인이 필요합니다.");
+                    continue;
+                }
                 System.out.print("삭제할 명언 번호 입력 > ");
                 int removeId = Integer.parseInt(sc.nextLine().trim());
+
                 for (int i = 0; i < wiseSayingList.size(); i++) {
                     if (wiseSayingList.get(i).getId() == removeId)
-                        wiseSayingList.remove(wiseSayingList.get(i));
+                        if (wiseSayingList.get(i).getUserId().equals(loginedMember.getUserId())) {
+                            wiseSayingList.remove(wiseSayingList.get(i));
+                            System.out.println(removeId + "번 명언 삭제 완료!!");
+                        } else {
+                            System.out.println("다른 사람이 작성한 게시물은 삭제할 수 없습니다.");
+                        }
                 }
-                System.out.println(removeId + "번 명언 삭제 완료!!");
             } else if (command.equals("수정")) {
+                if (loginedMember == null) {
+                    System.out.println("로그인이 필요합니다.");
+                    continue;
+                }
                 System.out.print("수정할 명언 번호 입력 > ");
                 int modifyId = Integer.parseInt(sc.nextLine().trim());
 
                 for (int i = 0; i < wiseSayingList.size(); i++) {
-                    if (wiseSayingList.get(i).getId() == modifyId) {
-                        System.out.println("기존 명언 : " + wiseSayingList.get(i).getContent());
-                        System.out.print("명언(수정) : ");
-                        String modifyContent = sc.nextLine().trim();
+                    if (wiseSayingList.get(i).getId() == modifyId)
+                        if (wiseSayingList.get(i).getUserId().equals(loginedMember.getUserId())) {
+                            System.out.println("기존 명언 : " + wiseSayingList.get(i).getContent());
+                            System.out.print("명언(수정) : ");
+                            String modifyContent = sc.nextLine().trim();
 
-                        System.out.println("기존 작가 : " + wiseSayingList.get(i).getAuthor());
-                        System.out.print("작가(수정) : ");
-                        String modifyAuthor = sc.nextLine().trim();
+                            System.out.println("기존 작가 : " + wiseSayingList.get(i).getAuthor());
+                            System.out.print("작가(수정) : ");
+                            String modifyAuthor = sc.nextLine().trim();
 
-                        wiseSayingList.get(i).setContent(modifyContent);
-                        wiseSayingList.get(i).setAuthor(modifyAuthor);
-                    }
+                            wiseSayingList.get(i).setContent(modifyContent);
+                            wiseSayingList.get(i).setAuthor(modifyAuthor);
+                            System.out.println(modifyId + "번 명언 수정 완료!!");
+                        } else {
+                            System.out.println("다른 사람이 작성한 게시물은 수정할 수 없습니다.");
+                        }
                 }
-                System.out.println(modifyId + "번 명언 수정 완료!!");
             } else if (command.equals("회원가입")) {
                 // 중복 아이디 검증
                 String userId;
@@ -104,10 +133,42 @@ public class App {
                 memberList.add(member);
                 System.out.println("회원가입 완료!!");
                 userPrimaryId++;
-            } else if (command.equals("회원목록")) {
-                System.out.println("회원번호  /  회원ID  /  회원PW  /  가입날짜");
-                for (int i = 0; i < memberList.size(); i++) {
-                    System.out.println(memberList.get(i).getId() + " / " + memberList.get(i).getUserId() + " / " + memberList.get(i).getPassword() + " / " + memberList.get(i).getRegDate());
+            } else if (command.equals("로그인")) {
+                if (loginedMember != null) {
+                    System.out.println("로그아웃을 먼저 해야합니다.");
+                    continue;
+                }
+                Member checkedMember = null;
+
+                System.out.print("(로그인)ID 입력 : ");
+                String userId = sc.nextLine().trim();
+                System.out.print("(로그인)PW 입력 : ");
+                String password = sc.nextLine().trim();
+
+                for (Member member : memberList) {
+                    if (userId.equals(member.userId)) {
+                        checkedMember = member;
+                        break;
+                    }
+                }
+
+                if (checkedMember == null) {
+                    System.out.println("존재하지 않는 ID 입니다.");
+                    continue;
+                } else if (!password.equals(checkedMember.password)) {
+                    System.out.println("비밀번호가 일치하지 않습니다.");
+                    continue;
+                }
+
+                loginedMember = checkedMember;
+
+                System.out.println("[" + checkedMember.getUserId() + "]님 환영합니다.");
+            } else if (command.equals("로그아웃")) {
+                if (loginedMember != null) {
+                    System.out.println("[" + loginedMember.getUserId() + "]님 로그아웃");
+                    loginedMember = null;
+                } else {
+                    System.out.println("로그인을 먼저 해야합니다.");
                 }
             }
         }
--- src/main/java/org/example/WiseSaying.java
+++ src/main/java/org/example/WiseSaying.java
@@ -1,27 +1,16 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
-    public WiseSaying(int id,String content,String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-    public int getId() {
-        return this.id;
-    }
-    public String getContent() {
-        return this.content;
-    }
-    public String getAuthor() {
-        return this.author;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    private String userId;
 }
